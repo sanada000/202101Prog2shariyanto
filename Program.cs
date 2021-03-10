9842c37aa4f5273930db4a02dde1a0ac134b2b41
sanada_000 <sanada_000@yahoo.com>
2021-03-10 10:09:59 +0700

    hworld

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _202101Prog2Shariyanto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
